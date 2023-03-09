67416574739d3f7565a982a822707b1d87515a02
Emilia Z <emilia.zalewska.gdn@gmail.com>
2023-03-09 01:41:52 +0100

    Adding task 2 and 3, changed first gallery picture

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash/throttle';
 
+const feedback = document.querySelector('.feedback-form');
+const { email, message } = feedback.elements;
+
+function saveFeedback(event) {
+  event.preventDefault();
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+loadFeedback();
+
+function loadFeedback() {
+  const receivedFeedback = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (receivedFeedback) {
+    email.value = receivedFeedback.email;
+    message.value = receivedFeedback.message;
+  }
+}
+
+function submitFeedback(event) {
+  event.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill out all fields!');
+  }
+  const receivedFeedback = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  const object = {
+    email: receivedFeedback.email,
+    message: receivedFeedback.message,
+  };
+  console.log(object);
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+feedback.addEventListener('input', throttle(saveFeedback, 500));
+feedback.addEventListener('submit', submitFeedback);
